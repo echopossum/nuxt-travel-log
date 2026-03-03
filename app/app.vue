f81ba4990243d95835b7bf8e1fba992756eba552
Echo <echopossum@gmail.com>
2026-03-02 22:11:00 -0500

    Setup tailwind, daisyui, and icons

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <h1 class="">Hello World</h1>
+    <button class="btn btn-primary">Test</button>
+    <Icon name="tabler:map" />
   </div>
 </template>
