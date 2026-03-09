3b8a2f58845dcb02faf5b9876c9b1119adfd70e8
Echo <echopossum@gmail.com>
2026-03-08 21:50:13 -0400

    Mess with SSR auth

--- app/stores/auth.ts
+++ app/stores/auth.ts
@@ -2,12 +2,17 @@ import { createAuthClient } from "better-auth/vue";
 const authClient = createAuthClient();
 
 export const useAuthStore = defineStore("authStore", () => {
-  const session = authClient.useSession();
-
-  const user = computed(() => session.value.data?.user);
-  const loading = computed(
-    () => session.value.isPending || session.value.isRefetching,
+  const session = ref<Awaited<ReturnType<typeof authClient.useSession>> | null>(
+    null,
   );
+
+  async function init() {
+    const data = await authClient.useSession(useFetch);
+    session.value = data;
+  }
+
+  const user = computed(() => session.value?.data?.user);
+  const loading = computed(() => session.value?.isPending);
 
   async function signIn() {
     await authClient.signIn.social({
@@ -22,5 +27,5 @@ export const useAuthStore = defineStore("authStore", () => {
     navigateTo("/");
   }
 
-  return { loading, signIn, user, signOut };
+  return { init, loading, signIn, user, signOut };
 });
