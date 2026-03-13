31657e3b315bfb4d17993adbdc50788d2413b770
Echo <echopossum@gmail.com>
2026-03-12 23:01:40 -0400

    Add front end and back end form validation for add location form

---
+++ app/components/app/form-field.vue
@@ -0,0 +1,17 @@
+<script lang="ts" setup>
+const props = defineProps<{
+    label: string,
+    name: string,
+    type: "text" | "textarea" | "number",
+    error?: string
+}>()
+</script>
+
+<template>
+    <fieldset class="fieldset">
+        <legend class="fieldset-legend">{{ props.label }}</legend>
+        <Field :name="props.name" :type="props.type" class="w-full"
+            :class="{ 'input-error': props.error, 'input': type === 'text' || type === 'number', 'textarea': type === 'textarea' }" />
+        <p v-if="props.error" class="fieldset-label text-error">{{ props.error }}</p>
+    </fieldset>
+</template>
--- app/pages/dashboard/add.vue
+++ app/pages/dashboard/add.vue
@@ -1,3 +1,48 @@
+<script lang="ts" setup>
+import { InsertLocation } from '~/lib/db/schema';
+import { toTypedSchema } from '@vee-validate/zod'
+import type { FetchError } from 'ofetch'
+
+
+
+
+
+
+const { handleSubmit, errors, meta } = useForm({
+    validationSchema: toTypedSchema(InsertLocation)
+})
+
+const onSubmit = handleSubmit(async (values) => {
+    try {
+        const insertedData = await $fetch("/api/locations", {
+            method: 'POST',
+            body: values
+        })
+        console.log(insertedData)
+    } catch (e) {
+        const error = e as FetchError
+        submitError.value = error.statusMessage || 'An unknown error occured'
+    }
+
+})
+
+onBeforeRouteLeave(() => {
+    if (meta.value.dirty) {
+        const confirm = window.confirm('Are you sure you want to leave? All unsaved changes will be lost.')
+        if (!confirm) {
+            return false
+        }
+    }
+    return true
+})
+
+const router = useRouter()
+
+const submitError = ref('')
+
+</script>
+
+
 <template>
     <div class="container max-w-md mx-auto">
         <div class="text-lg my-4">
@@ -8,25 +53,27 @@
                 specific times you visited this location after adding it.
             </p>
         </div>
-        <form>
-            <fieldset class="fieldset">
-                <legend class="fieldset-legend">Name</legend>
-                <input name="name" type="text" class="input w-full" />
-            </fieldset>
-            <fieldset class="fieldset">
-                <legend class="fieldset-legend">Description</legend>
-                <textarea name="description" type="text" class="textarea input w-full" />
-            </fieldset>
-            <fieldset class="fieldset">
-                <legend class="fieldset-legend">Latitude</legend>
-                <input name="lat" type="number" class="input w-full" />
-            </fieldset>
-            <fieldset class="fieldset">
-                <legend class="fieldset-legend">Longitude</legend>
-                <input name="lon" type="number" class="input w-full" />
-            </fieldset>
-            <div>
-                <button type="submit"></button>
+        <div v-if="submitError" role="alert" class="alert alert-error">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 shrink-0 stroke-current" fill="none"
+                viewBox="0 0 24 24">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                    d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
+            </svg>
+            <span>{{ submitError }}</span>
+        </div>
+        <form class="flex flex-col gap-2" @submit.prevent="onSubmit">
+            <AppFormField name="name" type="text" label="Name" :error="errors.name" />
+            <AppFormField name="description" type="textarea" label="Description" :error="errors.description" />
+            <AppFormField name="lat" type="number" label="Latitude" :error="errors.lat" />
+            <AppFormField name="long" type="number" label="Longitude" :error="errors.long" />
+            <div class="flex justify-end gap-2">
+                <button class="btn btn-outline" type="button" @click="router.back()">
+                    <Icon size="24" name="tabler:logout-2" />
+                    Cancel
+                </button>
+                <button class="btn btn-primary" type="submit">Add
+                    <Icon size="24" name="tabler:circle-plus-filled" />
+                </button>
             </div>
         </form>
     </div>
